dbc8c9162c2d7da28c38a2e1e12bfeeb19936ba9
arek-jozwiak-coderslab <Gonsie123>
2021-06-26 09:36:03 +0200

    fix app config

--- src/main/java/pl/coderslab/AppConfig.java
+++ src/main/java/pl/coderslab/AppConfig.java
@@ -4,50 +4,17 @@ package pl.coderslab;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import javax.persistence.EntityManagerFactory;
-import javax.validation.Validator;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab")
-@EnableTransactionManagement
-@EnableJpaRepositories
 public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
-    public LocalEntityManagerFactoryBean entityManagerFactory() {
-        LocalEntityManagerFactoryBean entityManagerFactoryBean
-                = new LocalEntityManagerFactoryBean();
-        entityManagerFactoryBean.setPersistenceUnitName("warjees26PersistenceUnit");
-        return entityManagerFactoryBean;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager jpaTransactionManager =
-                new JpaTransactionManager(entityManagerFactory);
-        return jpaTransactionManager;
-    }
 
     @Bean
     public ViewResolver viewResolver() {
@@ -64,23 +31,5 @@ public class AppConfig implements WebMvcConfigurer {
         configurer.enable();
     }
 
-    @Bean
-    public Validator validator() {
-        return new LocalValidatorFactoryBean();
-    }
 
-    @Bean(name = "localeResolver")
-    public LocaleContextResolver getLocaleContextResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("pl", "PL"));
-        return localeResolver;
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
-        stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain",
-                Charset.forName("UTF-8"))));
-        converters.add(stringConverter);
-    }
 }
